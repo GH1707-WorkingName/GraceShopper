a75805eca0930edb6f489b2e405eb8946f6f25d7
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-12 14:58:03 -0400

    completed authentication

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../db/user');
 
 
+
 router.post('/login', (req, res, next) =>{
   User.findOne({
     where: {
@@ -9,11 +10,36 @@ router.post('/login', (req, res, next) =>{
     }
   })
   .then(user => {
-    if (!user) res.sendStatus(401); 
-    // else {
-    //  if(user.correctPassword(req.body.password)) 
-    // }
+    if (!user) res.status(401).send('User not found');
+    else if (!user.correctPassword(req.body.password)) res.status(401).send('Incorrect password');
+    else { 
+      req.login(user, err => {
+        if (err) next(err);
+        else res.json(user);
+      });
+    }
   })
+  .catch(next);
+});
+
+router.post('/signup', (req, res, next) => {
+  User.create(req.body)
+  .then( user => {
+    req.login(user, err => {
+      if (err) next(err);
+      else res.json(user);
+    });
+  })
+  .catch(next);
+}); 
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  res.sendStatus(200);
+});
+
+router.get('/me', (req, res, next) =>{
+  res.json(req.user);
 })
 
 module.exports = router; 
