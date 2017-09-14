3e9486175bee547c0165023212b75556a8637e2c
cylau1031 <cylau1031@gmail.com>
2017-09-14 19:30:24 -0400

    feat: made Signup a connected component

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,3 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+export props => {
+  return (
+    <div>
+      <form name="signup-form" onSubmit={props.handleSubmit}>
+        <div>
+          <label>Email: </label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input type="text" name="password" />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    handleSubmit: () => {
+      console.log('HELLO')
+    }
+  }
+}
+
+const SignupContainer = connect(mapDispatchToProps)(Signup)
+export default SignupContainer
