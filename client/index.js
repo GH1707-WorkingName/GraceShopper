7a99b06ccd819b23721d02855424ca598f4171c0
cylau1031 <cylau1031@gmail.com>
2017-09-14 17:02:14 -0400

    fix: moved location of banner component to allproducts component

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import {fetchAllProducts} from './reducers';
 import store from './store';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import Banner from './components/Banner'
+
 // import navbar, footer, all Components
 
 export default class Index extends Component {
@@ -17,7 +17,6 @@ export default class Index extends Component {
     return (
       <div>
         <Navbar />
-        <Banner />
           <Switch>
             <Route exact path="/" component={AllProducts} />
           </Switch>
