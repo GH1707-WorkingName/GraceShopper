d51f2c5f5171e649db5effed68daa8bc5fafe97d
Kimberly Ton <kimberlyton@gmail.com>
2017-09-13 20:49:40 -0400

    pull down master to this branch

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './client/store.js';
+import Index from './client/';
 import './client/index.scss';
 
 ReactDOM.render(
   <Provider store = {store}>
-  <div>Hello, world!</div>
+    <Router>
+      <Index />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
---
+++ client/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom';
+import AllProducts from './components/AllProducts';
+import {fetchAllProducts} from './reducers';
+import store from './store';
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+// import navbar, footer, all Components
+
+export default class Index extends Component {
+  componentDidMount() {
+    store.dispatch(fetchAllProducts())
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Banner />
+          <Switch>
+            <Route exact path="/" component={AllProducts} />
+          </Switch>
+        <Footer />
+      </div>
+    )
+  }
+}
