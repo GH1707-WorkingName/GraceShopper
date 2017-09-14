1ba2adde77d9d5877a3ecab29fb6467ac1cb077b
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-14 17:01:06 -0400

    add bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import store from './client/store.js';
 import Index from './client/';
 import './client/index.scss';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(
   <Provider store = {store}>
