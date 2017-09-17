bddb50353722b0d8f988ee63f708e98d59703339
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-16 22:09:12 -0400

    add profile button to navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -9,6 +9,9 @@ export default () => {
         <Link to="/cart">
           <span className="glyphicon glyphicon-shopping-cart pull-right white"></span>
         </Link>
+        <Link to="/profile">
+        <span className="glyphicon glyphicon-user pull-right white"></span>
+        </Link>
         <Link to="/">
           <span className="glyphicon glyphicon-home pull-right white"></span>
         </Link>
@@ -17,6 +20,13 @@ export default () => {
           className="pull-right">
           <button type="button" className="btn btn-default btn-sm">
             Sign Up
+          </button>
+        </Link>
+        <Link 
+          to="/login"
+          className="pull-right">
+          <button type="button" className="btn btn-default btn-sm">
+            Login
           </button>
         </Link>
       </h1>
