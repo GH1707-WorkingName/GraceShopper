5f983d852fd5e225219a0ced209bdb2619e0b436
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-16 22:04:27 -0400

    signup, home, cart buttons in navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default () => {
   return (
     <nav>
-      <h1>REALITY BTYES</h1>
+      <h1>REALITY BYTES 
+        <Link to="/cart">
+          <span className="glyphicon glyphicon-shopping-cart pull-right white"></span>
+        </Link>
+        <Link to="/">
+          <span className="glyphicon glyphicon-home pull-right white"></span>
+        </Link>
+        <Link 
+          to="/signup"
+          className="pull-right">
+          <button type="button" className="btn btn-default btn-sm">
+            Sign Up
+          </button>
+        </Link>
+      </h1>
     </nav>
   )
 }
