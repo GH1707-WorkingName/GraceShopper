d1eaeeb14a33e9466d0113cf25770125e9806c0b
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-09-17 16:36:19 -0400

    show logout button only if user is logged in, only show login/signup if user has not signed in

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-
-export default () => {
+export const Navbar = ({user}) => {
   return (
     <nav>
       <h1>REALITY BYTES 
@@ -13,23 +13,43 @@ export default () => {
         <span className="glyphicon glyphicon-user pull-right white"></span>
         </Link>
         <Link to="/">
-          <span className="glyphicon glyphicon-home pull-right white"></span>
+          <span className="glyphicon glyphicon-home white"></span>
         </Link>
-        <Link 
-          to="/signup"
-          className="pull-right">
-          <button type="button" className="btn btn-default btn-sm">
-            Sign Up
-          </button>
-        </Link>
-        <Link 
-          to="/login"
-          className="pull-right">
-          <button type="button" className="btn btn-default btn-sm">
-            Login
-          </button>
-        </Link>
+        {
+          user.id ? (
+          <div>
+            <Link to="/logout" className="pull-right">
+              <button type="button" className="btn btn-default btn-sm">
+                Logout
+              </button>
+            </Link>
+          </div>
+          ) : (
+            <div>
+            <Link to="/signup" className="pull-right">
+              <button type="button" className="btn btn-default btn-sm">
+                Sign Up
+              </button>
+            </Link>
+            <Link to="/login" className="pull-right">
+              <button type="button" className="btn btn-default btn-sm">
+                Login
+              </button>
+            </Link>
+            </div>
+          )
+        }
       </h1>
     </nav>
   )
 }
+
+export const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+const NavbarContainer = connect(mapStateToProps)(Navbar);
+export default NavbarContainer; 
+
