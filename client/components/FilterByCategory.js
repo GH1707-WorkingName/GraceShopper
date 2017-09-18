5b1aee004a6d33777f22b5b68b62362a415144df
cylau1031 <cylau1031@gmail.com>
2017-09-18 07:13:05 -0400

    feat: inital commit

---
+++ client/components/FilterByCategory.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+export class FilterByCategory extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
