e403ce0438420cfb8bc642dc0ebfe1c8e3b511e9
Kimberly Ton <kimberlyton@gmail.com>
2017-09-20 11:54:30 -0400

    changed banner photo

--- client/components/Banner.js
+++ client/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return (
     <div className="col-md-12">
-      <img className="bannerImage" src="http://thebloggingrapper.com/wp-content/uploads/2014/07/Welcome-To-Reality.jpg" />
+      <img className="bannerImage" src="http://www.aromni.com/wp-content/uploads/2017/02/augmented-reality-potential-greater-than-you-think-61-1068x601.jpg" />
     </div>
   )
 }
